2ee9328602a106c73bc81929e33477d1fdc6392f
NisKuMo <nishant.mondal@gmail.com>
2024-07-07 02:03:06 -0700

    Idek

---
+++ src/processing.h
@@ -0,0 +1,16 @@
+//
+// Created by Nishant Mondal on 7/7/24.
+//
+
+#include "iostream"
+#include "string"
+
+#ifndef SUDOKUSOLVER_PROCESSING_H
+#define SUDOKUSOLVER_PROCESSING_H
+
+namespace SudokuSolver
+{
+    int** convert(std::string rawString, int gridLength);
+}
+
+#endif //SUDOKUSOLVER_PROCESSING_H
